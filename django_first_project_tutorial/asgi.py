b45f0626d68d1150828754c78f4d0b510d239333
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-18 17:33:47 +0000

    Formatter changes generated files (ruff format .)

--- django_first_project_tutorial/asgi.py
+++ django_first_project_tutorial/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_first_project_tutorial.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "django_first_project_tutorial.settings"
+)
 
 application = get_asgi_application()
--- django_first_project_tutorial/wsgi.py
+++ django_first_project_tutorial/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_first_project_tutorial.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "django_first_project_tutorial.settings"
+)
 
 application = get_wsgi_application()
