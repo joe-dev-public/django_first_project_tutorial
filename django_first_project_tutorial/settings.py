6180402d8f56c072f4d4d4b96ad66f8771e5825c
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-18 17:40:07 +0000

    Basic PostgreSQL DB engine settings

--- django_first_project_tutorial/settings.py
+++ django_first_project_tutorial/settings.py
@@ -74,9 +74,15 @@ WSGI_APPLICATION = "django_first_project_tutorial.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    # With the right local postgres settings, this minimal setup *seems* to
+    # allow the initial (tutorial) migration to run just fine.
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_first_project_tutorial",
     }
 }
 
