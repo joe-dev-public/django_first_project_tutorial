2280f636652e1a405732835f7b9667a467c5350c
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-19 17:58:52 +0000

    Purchasing: basic 'Release' & 'Item' models, first pass

--- purchasing/models.py
+++ purchasing/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Field: first arg = human-readable name, aka:
+# https://docs.djangoproject.com/en/4.2/topics/db/models/#verbose-field-names
+
+
+class Release(models.Model):
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#charfield
+    title = models.CharField("release title", max_length=200)
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#datefield
+    date = models.DateField("date released")
+
+
+class Item(models.Model):
+    url = models.CharField("item URL", max_length=400)
+    total_price = models.DecimalField("total price", max_digits=6, decimal_places=2)
+    track_price = models.DecimalField("track price", max_digits=6, decimal_places=2)
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#foreignkey
+    # Not sure I'd actually want deleting a Release to delete an Item, but
+    # let's just leave it like this for now.
+    release = models.ForeignKey(Release, on_delete=models.CASCADE)
