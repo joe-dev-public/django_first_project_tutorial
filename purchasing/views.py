b917d02c908a072e9a79ebe51073beed92b1b70e
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-24 21:05:16 +0000

    Purchasing: basic Releases model query in view

--- purchasing/views.py
+++ purchasing/views.py
@@ -3,13 +3,17 @@
 from django.http import HttpResponse
 from datetime import datetime
 
+from .models import Release
+
 # Interesting to note that datetime.now() evaluates to the current time that
 # the dev server (re)loads, rather than when the user loads the page ;¬)
 content = "Hello world, " + str(datetime.now())
 
 
 def index(request):
-    return HttpResponse(content)
+    last_fifty_releases = Release.objects.order_by("-added")[:50]
+    output = "<br>".join([r.title for r in last_fifty_releases])
+    return HttpResponse(content + "<br><br>Last 50 releases:<br><br>" + output)
 
 
 def item(request, item_id):
